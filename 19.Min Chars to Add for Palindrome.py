af13cb631e82e289c8037a746deafdcd58238f27
PAWAN G <121444830+pawang1745@users.noreply.github.com>
2024-12-03 21:36:57 +0530

    19.Min Chars to Add for Palindrome.py

---
+++ 19.Min Chars to Add for Palindrome.py
@@ -0,0 +1,33 @@
+class Solution:
+    def computeLPSArray(self,pat):
+        n = len(pat)
+        
+        lps = [0] * n
+    
+        len_lps = 0
+    
+        i = 1
+        while i < n:
+            
+            if pat[i] == pat[len_lps]:
+                len_lps += 1
+                lps[i] = len_lps
+                i += 1
+            
+            else:
+              
+                if len_lps != 0:
+                    len_lps = lps[len_lps - 1]
+                    
+                else:
+                    lps[i] = 0
+                    i += 1
+        return lps
+        
+    def minChar(self, s):
+        
+        n = len(s)
+        rev = s[::-1]
+        s = s + "$" + rev
+        lps = self.computeLPSArray(s)
+        return n - lps[-1]
